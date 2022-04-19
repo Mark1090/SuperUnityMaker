efd452979126dda4c939348b1bfa068d3ddb43a2
DevMarc1 <marc.meronome@vilatzara.com>
2022-04-19 11:33:12 +0200

    pitos

---
+++ Assets/Scripts/UnknownPart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnknownPart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+             Destroy(this.gameObject);
+    }
+}
