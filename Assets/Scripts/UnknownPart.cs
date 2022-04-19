976bb9d7b2741be3fdb7e28b4cc6dcfd3127720e
DevMarc1 <marc.meronome@vilatzara.com>
2022-04-19 12:14:23 +0200

    sum

--- Assets/Scripts/UnknownPart.cs
+++ Assets/Scripts/UnknownPart.cs
@@ -7,18 +7,23 @@ public class UnknownPart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var col = gameObject.GetComponent<Renderer> ().material.color;
+        col.a = 0.2f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    void OnTriggerEnter2D(Collider2D coll)
-    {
-        if (coll.gameObject.tag == "Player")
-             Destroy(this.gameObject);
-    }
+   // void OnTriggerEnter2D(Collider2D coll)
+   // {
+    //    if (coll.gameObject.tag == "Player")
+    //    {
+            // Destroy(this.gameObject);
+           // for (int i = 255; i = 0; i--)
+           // gameObject.GetComponent<Renderer>().material.color.a = i;
+     //   }
+   // }
 }
