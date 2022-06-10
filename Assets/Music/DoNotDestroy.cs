505dc748e4c30566f55ab724bf0dfb317d999953
DevMarc1 <marc.meronome@vilatzara.com>
2022-06-10 11:14:35 +0200

    u

--- Assets/Music/DoNotDestroy.cs
+++ Assets/Music/DoNotDestroy.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class DoNotDestroy : MonoBehaviour
 {
+	public float length;
+	private static Audio Source instance;
 	private void Awake()
 	{
-		GameObject[] musicObj = GameObject.FindGameObjectWithTag("Music");
-		if(musicObj.Length > 1)
+		GameObject musicObj = GameObject.FindGameObjectWithTag("Music");
+		if(instance != null)
 		{	
 			Destroy(this.gameObject);
 		}
+		else 
+		{
 		DontDestroyOnLoad(this.gameObject);
+		}
 	}
 }    
+			
